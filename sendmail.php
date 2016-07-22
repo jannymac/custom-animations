061936d1dc61c62a7bee19c6a592fc11db7ac796
jannymac <jannymac@users.noreply.github.com>
2016-07-21 23:54:38 -0400

    Update sendmail.php
    So this version output the variables that were passed to it, so variables being received and able to be output..so, moving on...

--- sendmail.php
+++ sendmail.php
@@ -1,11 +1,18 @@
 <?php
 
-$name = $_REQUUEST['name'];
-$email = $_REQUUEST['email'];
-$phonenumber = $_REQUUEST['phonenumber'];
-$vidtype = $_REQUUEST['vidtype'];
-$length = $_REQUUEST['length'];
-$addinfo = $_REQUUEST['addinfo'];
-$discovery = $_REQUUEST['discovery'];
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$phonenumber = $_REQUEST['phonenumber'];
+$vidtype = $_REQUEST['vidtype'];
+$length = $_REQUEST['length'];
+$addinfo = $_REQUEST['addinfo'];
+$discovery = $_REQUEST['discovery'];
 
+echo $name."<br/>";
+echo $email."<br/>";
+echo $phonenumber."<br/>";
+echo $vidtype."<br/>";
+echo $length."<br/>";
+echo $addinfo."<br/>";
+echo $discovery."<br/>";
 ?>
